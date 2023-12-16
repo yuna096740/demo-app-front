edaf058fcdfb26f1ec0d8ed201bfddb455bd5950
yuna096740 <yyuusseeii1111@gmail.com>
2023-12-16 10:01:40 +0900

    Deploy test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import axios from "axios";
 
 function App() {
+  // const [data, setData] = React.useState();
+  // const url = "http://127.0.0.1:8000/ping";
+
+  // const GetData = () => {
+  //   axios.get(url).then((res) => {
+  //     setData(res.data);
+  //   });
+  // };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Deploy Test in Vercel</div>
+      {/* {data ? (
+        <div>{data.data}</div>
+      ) : (
+        <button onClick={GetData}>データを取得</button>
+      )} */}
     </div>
   );
 }
